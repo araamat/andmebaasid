31003aa4b901892d5228acef8ab44d062acf6598
araamat <raamatandrus@gmail.com>
2024-05-20 15:09:06 +0300

    yl21 3ja4 tehtud

--- ylesanded.sql
+++ ylesanded.sql
@@ -5,3 +5,17 @@ FROM books
 WHERE release_date >= '2010' AND type = 'new'
 ORDER BY title ASC;
 
+-- yl21-3
+SELECT title, release_date, price, type
+FROM books
+WHERE release_date < 1970
+  AND price < 20
+  AND type = 'used';
+
+-- yl21-4
+SELECT YEAR(order_date) AS Aasta, COUNT(*) AS 'Tellimuste arv'
+FROM orders
+GROUP BY YEAR(order_date);
+
+
+
